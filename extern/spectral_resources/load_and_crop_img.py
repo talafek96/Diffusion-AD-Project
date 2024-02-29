bee8e811bb07320cc8c14e9dc76bd83fa24e3472
Amit <amitir22@gmail.com>
2024-02-29 06:34:04 +0200

    SPECTRAL: Segmenting and Stitching works!

--- extern/spectral_resources/load_and_crop_img.py
+++ extern/spectral_resources/load_and_crop_img.py
@@ -29,6 +29,7 @@ def show_bands(list_of_bands, HDR):
                         HDR.read_band(list_of_bands[2])], axis=-1)
         debug = th.from_numpy(rgb)
     # rgb = rgb / rgb.max() * 1.5
+    print(rgb.shape)  # TODO: DELETE
 
     figure = plt.figure()
     figure.canvas.manager.set_window_title("RGB Image")
@@ -54,7 +55,8 @@ def read_bands_of_HDR_as_tensor(list_of_bands: List, image_path: str):
 def get_sliced_bands_of_HDR_as_256x256_tensor(list_of_bands: List, image_path: str, center: Tuple[int, int]):
     im_tensor = read_bands_of_HDR_as_tensor(list_of_bands, image_path)
 
-    size_x, size_y, num_channels = im_tensor.shape[0], im_tensor.shape[1], im_tensor.shape[2]
+    size_y, size_x, num_channels = im_tensor.shape[0], im_tensor.shape[1], im_tensor.shape[2]
+    print(f"get_sliced_bands_of_HDR_as_256x256_tensor im_tensor.shape = {im_tensor.shape}")  #TODO: DELETE
 
     assert num_channels == 3
 
@@ -67,18 +69,110 @@ def get_sliced_bands_of_HDR_as_256x256_tensor(list_of_bands: List, image_path: s
     upper_y_slice = int(center_y + 256/2)
 
     if lower_x_slice < 0 or upper_x_slice > size_x or lower_y_slice < 0 or upper_y_slice > size_y:
-        raise OutOfBoundsError("You have to select a center point that is at least 256/2 pixels from the bounds of the image")
+        raise OutOfBoundsError(f"You have to select a center point that is at least 256/2 pixels from the bounds of the image\n\
+                               given (center_x, center_y) = {(center_x, center_y)}, (size_x, size_y) = {(size_x, size_y)}")
     
-    sliced_im_tensor = im_tensor[lower_x_slice: upper_x_slice, lower_y_slice: upper_y_slice, :]
+    sliced_im_tensor = im_tensor[lower_y_slice: upper_y_slice, lower_x_slice: upper_x_slice, :]
 
     return sliced_im_tensor
+
+
+def get_next_center_point_of_spectral_image(spectral_im_tensor: th.Tensor):
+    """
+    Usage:
+    >>>  spectral_im_tensor = read_bands_of_HDR_as_tensor(list_of_bands, spectral_image_path)
+    >>>  for current_center in get_next_center_point_of_spectral_image(spectral_im_tensor):
+    >>>      logic(current_center)
+    >>>      # e.g:
+    >>>      heatmap, anomaly_score = find_anomalies(list_of_bands, spectral_image_path, current_center)
+    """
+    size_y, size_x, num_channels = spectral_im_tensor.shape[0], spectral_im_tensor.shape[1], spectral_im_tensor.shape[2]
+    
+    assert num_channels == 3
+
+    # ROW -> y
+    # COL -> x
+    num_segment_rows = int(np.ceil(size_y / 256.0))
+    num_segment_columns = int(np.ceil(size_x / 256.0))
+
+    initial_center_y = 128
+    initial_center_x = 128
+    current_center_y = initial_center_y
+    current_center_x = initial_center_x
+
+    for row_index in range(num_segment_rows):
+        for column_index in range(num_segment_columns):
+            current_center_y = min(initial_center_y + 256 * row_index, size_y - 128)
+            current_center_x = min(initial_center_x + 256 * column_index, size_x - 128)
+            
+            yield (current_center_x, current_center_y)
+
+
+# # Example usage:
+# # Assuming 'segments' is a list of torch.Tensor segments
+"""
+with shape [3, 256, 256]
+>>> size_x = 1024  # Original image width
+>>> size_y = 768   # Original image height
+>>> stitched_result = stitch_segments(segments, size_x, size_y)
+"""
+def stitch_segments(segments, size_x, size_y):
+    """
+    Stitches image segments back into the original tensor.
+
+    Args:
+        segments (list of torch.Tensor): List of image segments (shape: [3, 256, 256]).
+        size_x (int): Original image width.
+        size_y (int): Original image height.
+
+    Returns:
+        torch.Tensor: Stitched image tensor (shape: [3, size_x, size_y]).
+    """
+    # Initialize an empty tensor for the stitched image
+    stitched_image = th.zeros(size_x, size_y)
+
+    # Calculate the number of segments in each dimension
+    num_segment_columns = int(np.ceil(size_x / 256.0))
+    num_segment_rows = int(np.ceil(size_y / 256.0))
+
+    # # Iterate over segments and copy content into the stitched image
+    # for i in range(num_segments_x):
+    #     for j in range(num_segments_y):
+    #         segment = segments[i * num_segments_y + j]
+    #         stitched_image[:, i * 256 : (i + 1) * 256, j * 256 : (j + 1) * 256] = segment
+    for row_index in range(num_segment_rows):
+        for col_index in range(num_segment_columns):
+            segment = segments[col_index * num_segment_rows + row_index]
+
+            # If this is the last row
+            if row_index == num_segment_rows - 1:
+                min_row, max_row = size_y - 256, size_y
+            else:
+                min_row, max_row = row_index * 256, (row_index+1) * 256
+
+            # If this is the last column
+            if col_index == num_segment_columns - 1:
+                min_col, max_col = size_x - 256, size_x
+            else:
+                min_col, max_col = col_index * 256, (col_index+1) * 256
+                
+            assert max_row - min_row == 256
+            assert max_col - min_col == 256
+            # "[:" to select all channels
+            # from min_row to max_row
+            # and from min_col to max_col
+            stitched_image[min_col : max_col, min_row : max_row] = segment
+
+    return stitched_image
 
 
 def main():
     HDR_swir = envi.open(HDR_PATH_SWIR, IMAGE_PATH_SWIR)
     # im_swir_rgb = show_img(HDR_swir)  # differnt band in each chunnel
     im_swir_single_band = show_bands([10, 20, 40], HDR_swir)# same band in each chunnel
-    print(get_sliced_bands_of_HDR_as_256x256_tensor([1, 10, 20], IMAGE_PATH_SWIR, (510, 130)))
+    print(get_sliced_bands_of_HDR_as_256x256_tensor([1, 10, 20], IMAGE_PATH_SWIR, (460, 220)))
+    plt.imshow(get_sliced_bands_of_HDR_as_256x256_tensor([1, 10, 20], IMAGE_PATH_SWIR, (460, 220)))
+    plt.show()
 
 
 if __name__ == '__main__':
--- extern/spectral_resources/spectral_denoising_api.py
+++ extern/spectral_resources/spectral_denoising_api.py
@@ -13,10 +13,13 @@ from utils.error_map import ErrorMapGenerator, BatchFilteredSquaredError
 from utils.anomaly_scorer import AnomalyScorer, MaxValueAnomalyScorer
 from extern.guided_diffusion.guided_diffusion import gaussian_diffusion as gd
 from extern.spectral_resources.load_and_crop_img import get_sliced_bands_of_HDR_as_256x256_tensor, \
+    get_next_center_point_of_spectral_image, read_bands_of_HDR_as_tensor, stitch_segments, \
     IMAGE_PATH_SWIR
 
 
 DEBUG = True
+VISUAL_CLIP_MIN_FACTOR = 50
+VISUAL_CLIP_MAX_FACTOR = 0.9
 
 
 def log(msg):
@@ -24,141 +27,208 @@ def log(msg):
         print(msg)
 
 
-def prepare_input_for_model(improper_input): #  TODO: rename param
-    permutated_bands = improper_input.permute(2, 0, 1).to('cuda')
-    prepared_input_as_batch = permutated_bands.unsqueeze(0)
+class SpectralAnomalyMarker:
+    is_model_loaded: bool
 
-    return prepared_input_as_batch
+    def __init__(self) -> None:
+        self.is_model_loaded = False
+        pass  # TODO:
 
+    def prepare_input_for_model(self, improper_input): #  TODO: rename param
+        permutated_bands = improper_input.permute(2, 0, 1).to('cuda')
+        prepared_input_as_batch = permutated_bands.unsqueeze(0)
 
-def load_model():
-    model_loader = ModelLoader()
-    model, diffusion = model_loader.get_model('256x256_uncond', path='models/256x256_diffusion_uncond.pt')
+        return prepared_input_as_batch
 
-    return model, diffusion
-
-
-def show_heatmap(heat_map_t, with_overlay=False, origin_t=None):
-    if not with_overlay:
-        display(heat_map_t)  # TODO
-
-
-def find_anomalies(selected_bands, image_path, center_point, num_noising_timesteps=100, 
-                   reconstruct_batch_size=16, should_use_direct_denoiser=False, 
-                   should_display_progress=True):
-    """
-    @param selected_bands: list of 3 integers indicating the selected channels to process
-    @param image_path: path to spectral image
-    @param center_point: a tuple of 2 integers indicating the center point of the 256x256 slice.
-                         e.g: center_point = (128, 128) will get you the leftmost top segment.
-    @param num_noising_timesteps: an integer indicating the number of noising steps applied.
-    @param reconstruct_batch_size: the number of times a noising-reconstructing process is going 
-                                   to happen before averaging the differences.
-    @param should_use_direct_denoiser: a boolean flag s.t when True - applies a faster but less 
-                                       accurate denoising
-    @param should_display_progress: a boolean flag indicating the display of tqdm and 
-                                    reconstructed images.
-
-    @returns (Tensor): a heatmap tensor that signifies the difference from the average 
-                       reconstruction and the original image.
-    """
-    if torch.cuda.is_available():
-        log('CUDA: On')
-    
-    log('loading selected bands to tensor...')
-    input_t = get_sliced_bands_of_HDR_as_256x256_tensor(selected_bands, image_path, center_point)
-    log('SUCCESS.')
-    
-    log('loading model...')
-    unet_model, guided_diffusion = load_model()
-    log('SUCCESS.')
-
-    noiser = TimestepUniformNoiser(guided_diffusion)
-
-    if should_use_direct_denoiser:
-        log('Direct denoiser is chosen.')
-        log('speed: +, accuracy: -')
-        denoiser = ModelTimestepDirectDenoiser(unet_model, guided_diffusion)
-    else:
-        log('Diffusion denoiser is chosen.')
-        log('speed: -, accuracy: +')
-        denoiser = ModelTimestepUniformDenoiser(unet_model, guided_diffusion)
-    
-    error_map_gen = BatchFilteredSquaredError()  # can be inherited from
-    anomaly_scorer = MaxValueAnomalyScorer()  # can be inherited from
-    
-    reconstructed_batch = []
-
-    for i in tqdm(range(reconstruct_batch_size)):
-        current_timesteps = torch.randint(low=int(num_noising_timesteps * 0.9),
-                                          high=int(num_noising_timesteps * 1.1),
-                                          size=[1]).item()
-
-        reconstructed_image_cpu = process_image(num_noising_timesteps, reconstruct_batch_size, 
-                                                should_display_progress, input_t, noiser, denoiser, 
-                                                reconstructed_batch, i, current_timesteps)
+    def load_model(self):
+        model_loader = ModelLoader()
         
-    # Aggregate results into a single tensor
-    device = reconstructed_batch[0].device
-    reconstructed_batch = torch.stack(reconstructed_batch).to(device)
+        if not self.is_model_loaded:
+            self.unet_model, self.guided_diffusion = model_loader.get_model('256x256_uncond', path='models/256x256_diffusion_uncond.pt')
+            self.is_model_loaded = True
 
-    anomaly_map, anomaly_score = evaluate_anomaly(((input_t.permute(2, 0, 1) / 2) + 0.5).cuda(),
-                                                  reconstructed_batch,
-                                                  error_map_gen,
-                                                  anomaly_scorer)
+    def find_anomalies(self, input_t, num_noising_timesteps=100, reconstruct_batch_size=16, 
+                       should_use_direct_denoiser=False, should_display_progress=True):
+        """
+        @param selected_bands: list of 3 integers indicating the selected channels to process
+        @param image_path: path to spectral image
+        @param center_point: a tuple of 2 integers indicating the center point of the 256x256 slice.
+                             e.g: center_point = (128, 128) will get you the leftmost top segment.
+        @param num_noising_timesteps: an integer indicating the number of noising steps applied.
+        @param reconstruct_batch_size: the number of times a noising-reconstructing process is going 
+                                       to happen before averaging the differences.
+        @param should_use_direct_denoiser: a boolean flag s.t when True - applies a faster but less 
+                                           accurate denoising
+        @param should_display_progress: a boolean flag indicating the display of tqdm and 
+                                        reconstructed images.
 
-    return anomaly_map, anomaly_score
+        @returns (Tensor): a heatmap tensor that signifies the difference from the average 
+                           reconstruction and the original image.
+        """
+        if torch.cuda.is_available():
+            log('CUDA: On')
 
+        log('loading model...')
+        self.load_model()
+        log('SUCCESS.')
 
-# generated using "extract method" TODO: reorder parameters so it's more readable
-def process_image(num_noising_timesteps, reconstruct_batch_size, should_display_progress, 
-                  input_t, noiser, denoiser, reconstructed_batch, i, current_timesteps):
-    log('applying noise...')
-    noised_image = noiser.apply_noise(input_t.unsqueeze(0), current_timesteps).squeeze(0)
-    log(f'SUCCESS. {i}/{reconstruct_batch_size}')
+        noiser = TimestepUniformNoiser(self.guided_diffusion)
 
-    prepared_input = prepare_input_for_model(noised_image)
+        if should_use_direct_denoiser:
+            log('Direct denoiser is chosen.')
+            log('speed: +, accuracy: -')
+            denoiser = ModelTimestepDirectDenoiser(self.unet_model, self.guided_diffusion)
+        else:
+            log('Diffusion denoiser is chosen.')
+            log('speed: -, accuracy: +')
+            denoiser = ModelTimestepUniformDenoiser(self.unet_model, self.guided_diffusion)
 
-    log('applying denoise... (using the model)')
-    with torch.no_grad():
-        reconstructed_image = denoiser.denoise(prepared_input, current_timesteps, 
+        error_map_gen = BatchFilteredSquaredError()  # can be inherited from
+        anomaly_scorer = MaxValueAnomalyScorer()  # can be inherited from
+
+        reconstructed_batch = []
+
+        for i in tqdm(range(reconstruct_batch_size)):
+            current_timesteps = torch.randint(low=int(num_noising_timesteps * 0.9),
+                                              high=int(num_noising_timesteps * 1.1),
+                                              size=[1]).item()
+            reconstructed_image = self.process_image(reconstruct_batch_size, i, current_timesteps, 
+                                                     input_t, noiser, denoiser, 
+                                                     should_display_progress)
+            reconstructed_batch.append(reconstructed_image)
+
+        # Aggregate results into a single tensor
+        device = reconstructed_batch[0].device
+        reconstructed_batch = torch.stack(reconstructed_batch).to(device)
+
+        anomaly_map, anomaly_score = self.evaluate_anomaly(((input_t.permute(2, 0, 1) / 2) + 0.5),
+                                                           reconstructed_batch,
+                                                           error_map_gen,
+                                                           anomaly_scorer)
+
+        return anomaly_map, anomaly_score
+
+    def process_image(self, reconstruct_batch_size, index_in_batch, current_timesteps, input_t, 
+                      noiser, denoiser, should_display_progress):
+        log('applying noise...')
+        noised_image = noiser.apply_noise(input_t.unsqueeze(0), current_timesteps).squeeze(0)
+        log(f'SUCCESS. {index_in_batch + 1}/{reconstruct_batch_size}')
+
+        prepared_input = self.prepare_input_for_model(noised_image)
+
+        log('applying denoise... (using the model)')
+        with torch.no_grad():
+            reconstructed_image = denoiser.denoise(prepared_input, current_timesteps, 
                                                    show_progress=should_display_progress)
-        reconstructed_batch.append(reconstructed_image)
 
-        log(f'SUCCESS. {i}/{reconstruct_batch_size}')
+            log(f'SUCCESS. {index_in_batch + 1}/{reconstruct_batch_size}')
 
-        if should_display_progress:
-            reconstructed_image_cpu = ((reconstructed_image.squeeze(0).cpu() / 2) + 0.5).clip(0, 1)
-            plt.imshow(reconstructed_image_cpu.permute(1, 2, 0))
-            plt.show()
+            reconstructed_image_cpu = \
+                ((reconstructed_image.squeeze(0).cpu() / 2) + 0.5).clip(0, 1)
 
-    return reconstructed_image_cpu
+            if should_display_progress:
+                plt.imshow(reconstructed_image_cpu.permute(1, 2, 0))
+                plt.show()
+
+            # return reconstructed_image.squeeze(0).cpu()
+            return reconstructed_image_cpu
+
+    def show_heatmap(self, heat_map_t, with_side_by_side=False, original=None, origin_t=None):
+        if with_side_by_side:
+            if original == None:
+                print("show_heatmap with_side_by_side requires an original tensor")
+            plt.subplot(1, 2, 1)
+            plt.imshow(original)
+        visual_clip_min = heat_map_t.min().item() * VISUAL_CLIP_MIN_FACTOR
+        visual_clip_max = heat_map_t.max().item() * VISUAL_CLIP_MAX_FACTOR
+        
+        plt.subplot(1, 2, 2)
+        plt.imshow(heat_map_t, cmap='hot', interpolation='nearest', 
+                   vmin=visual_clip_min, vmax=visual_clip_max)
+        plt.show()
+
+    def evaluate_anomaly(self, img: torch.Tensor,
+                         reconstructed_batch: torch.Tensor,
+                         error_map_gen: ErrorMapGenerator,
+                         anomaly_scorer: AnomalyScorer) -> Tuple[torch.Tensor, float, torch.Tensor]:
+        """
+        Return:
+        -------
+        anomaly_map, anomaly_score
+        """
+        # Calculate an anomaly map using all of the results
+        anomaly_map = error_map_gen.generate(img, reconstructed_batch)
+        anomaly_score = anomaly_scorer.score(anomaly_map)
+
+        return anomaly_map, anomaly_score
 
 
-def display_anomaly_map(anomaly_map):
-    # TODO: notice how vmin is defined. it's hardcoded for now but should actually be a hyperparameter
-    plt.imshow(anomaly_map, cmap='hot', interpolation='nearest', vmin=anomaly_map.min().item() * 50, max=anomaly_map.max().item())
+IS_QUIET_TESTS = True
 
 
-def evaluate_anomaly(img: torch.Tensor,
-                     reconstructed_batch: torch.Tensor,
-                     error_map_gen: ErrorMapGenerator,
-                     anomaly_scorer: AnomalyScorer) -> Tuple[torch.Tensor, float, torch.Tensor]:
-    """
-    Return:
-    -------
-    anomaly_map, anomaly_score
-    """
-    # Calculate an anomaly map using all of the results
-    anomaly_map = error_map_gen.generate(img, reconstructed_batch)
-    anomaly_score = anomaly_scorer.score(anomaly_map)
-
-    return anomaly_map, anomaly_score
+def test_diffusion(anomaly_marker: SpectralAnomalyMarker):
+    log('loading selected bands to tensor...')
+    input_t = get_sliced_bands_of_HDR_as_256x256_tensor([10, 20, 40], IMAGE_PATH_SWIR, 
+                                                        (460, 220))
+    log('SUCCESS.')
+    
+    heatmap, anomaly_score = anomaly_marker.find_anomalies(input_t, 150, 
+                                                           should_display_progress=not IS_QUIET_TESTS)
+    print(f"anomaly_score: {anomaly_score}")
+    anomaly_marker.show_heatmap(heatmap)
 
 
-def test():
-    model_input = prepare_input_for_model([10, 20, 40], IMAGE_PATH_SWIR, (130, 510))
+def test_direct(anomaly_marker: SpectralAnomalyMarker):
+    log('loading selected bands to tensor...')
+    input_t = get_sliced_bands_of_HDR_as_256x256_tensor([10, 20, 40], IMAGE_PATH_SWIR, 
+                                                        (460, 220))
+    log('SUCCESS.')
+
+    heatmap, anomaly_score = anomaly_marker.find_anomalies(input_t, 150, 
+                                                           should_display_progress=not IS_QUIET_TESTS, 
+                                                           should_use_direct_denoiser=True)
+    print(f"anomaly_score: {anomaly_score}")
+    anomaly_marker.show_heatmap(heatmap)
+
+
+def test_segment_iterator_direct(anomaly_marker: SpectralAnomalyMarker):
+    list_of_bands = [10, 20, 40]
+    spectral_image_path = IMAGE_PATH_SWIR
+    spectral_im_tensor = read_bands_of_HDR_as_tensor(list_of_bands, spectral_image_path)
+
+    heatmaps = []
+    anomaly_scores = []
+
+    for current_center in get_next_center_point_of_spectral_image(spectral_im_tensor):
+        log('loading selected bands to tensor...')
+        input_t = get_sliced_bands_of_HDR_as_256x256_tensor(list_of_bands, 
+                                                            spectral_image_path, 
+                                                            current_center)
+        log('SUCCESS.')
+
+        heatmap, anomaly_score = anomaly_marker.find_anomalies(input_t, 
+                                                               should_display_progress=not IS_QUIET_TESTS, 
+                                                               should_use_direct_denoiser=True)
+        heatmaps.append(heatmap)
+        anomaly_scores.append(anomaly_score)
+    
+    stitched_heatmap = stitch_segments(heatmaps, 
+                                       size_x=spectral_im_tensor.shape[0], 
+                                       size_y=spectral_im_tensor.shape[1])  # weird... i know
+    
+    # TODO: something better and quieter like to files
+    print(f"summarrized anomaly_score's: {sum(anomaly_scores)}")  
+    anomaly_marker.show_heatmap(stitched_heatmap, with_side_by_side=True, original=spectral_im_tensor) 
+
+
+def run_tests():
+    anomaly_marker = SpectralAnomalyMarker()
+    test_segment_iterator_direct(anomaly_marker)
+    # test_direct(anomaly_marker)
+    # test_diffusion(anomaly_marker)
 
 
 if __name__ == '__main__':
-    test()
+    DEBUG = True
+    IS_QUIET_TESTS = True
+    run_tests()
